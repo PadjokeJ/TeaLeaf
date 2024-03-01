4a90ceaafe7da2ff03e2b245c31757f74c76f4df
PadjokeJ <jonatanr.pfister@gmail.com>
2024-03-01 14:37:32 +0100

    Added missing jungle items and translations

--- src/main/java/io/itch/padjokej/tealeaf/item/ModItems.java
+++ src/main/java/io/itch/padjokej/tealeaf/item/ModItems.java
@@ -15,11 +15,13 @@ public class ModItems
             new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
     public static final Item ACACIA_TEALEAF = registerItem("acacia_tea_leaf",
             new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
+    public static final Item JUNGLE_TEALEAF = registerItem("jungle_tea_leaf",
+            new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
     public static final Item BIRCH_TEALEAF = registerItem("birch_tea_leaf",
             new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
     public static final Item DARK_OAK_TEALEAF = registerItem("dark_oak_tea_leaf",
             new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
-    public static final Item MANGROVE_OAK_TEALEAF = registerItem("mangrove_oak_tea_leaf",
+    public static final Item MANGROVE_OAK_TEALEAF = registerItem("mangrove_tea_leaf",
             new Item(new FabricItemSettings().group(ItemGroup.BREWING)));
     public static final Item SPRUCE_TEA = registerItem("spruce_tea",
             new DrinkableItem(new FabricItemSettings().group(ItemGroup.BREWING)));
@@ -27,11 +29,13 @@ public class ModItems
             new DrinkableItem(new FabricItemSettings().group(ItemGroup.BREWING)));
     public static final Item ACACIA_TEA = registerItem("acacia_tea",
             new DrinkableItem(new FabricItemSettings().group(ItemGroup.BREWING)));
+    public static final Item JUNGLE_TEA = registerItem("jungle_tea",
+            new DrinkableItem(new FabricItemSettings().group(ItemGroup.BREWING)));
     public static final Item BIRCH_TEA = registerItem("birch_tea",
             new DrinkableItem(new FabricItemSettings().group(ItemGroup.BREWING)));
     public static final Item DARK_OAK_TEA = registerItem("dark_oak_tea",
             new DrinkableItem(new FabricItemSettings().group(ItemGroup.BREWING)));
-    public static final Item MANGROVE_OAK_TEA = registerItem("mangrove_oak_tea",
+    public static final Item MANGROVE_OAK_TEA = registerItem("mangrove_tea",
             new DrinkableItem(new FabricItemSettings().group(ItemGroup.BREWING)));
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(TeaLeaf.MOD_ID,name), item);
